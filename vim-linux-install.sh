9c21c9e4931392c9bc9c608f8789ea77089dc5de
anton-ramanau <79052305+anton-ramanau@users.noreply.github.com>
2023-07-04 06:38:18 +0200

    Update vim-linux-install.sh

--- vim-linux-install.sh
+++ vim-linux-install.sh
@@ -10,4 +10,8 @@ if ! command -v vim >/dev/null 2>&1; then
     apt-get -y install vim
 fi
 
+if ! command -v git >/dev/null 2>&1; then
+    apt-get -y install git
+fi
+
 curl -f -o ~/.vimrc https://raw.githubusercontent.com/anton-ramanau/vim-custom/main/.vimrc
